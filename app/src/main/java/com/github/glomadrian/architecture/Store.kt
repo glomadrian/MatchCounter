c0f38244f754ab1b302bc51a97197b5f96d95386
Adrian Garcia <glomadrian@gmail.com>
2020-05-07 20:28:37 +0200

    Implement store

---
+++ app/src/main/java/com/github/glomadrian/architecture/Store.kt
@@ -0,0 +1,34 @@
+package com.github.glomadrian.architecture
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.github.glomadrian.background
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.scan
+
+class Store<A : Action, R : Result, S : State>(
+    val actionExecutor: ActionExecutor<A, R>,
+    private val reducer: Reducer<R, S>,
+    private val initState: S
+) {
+    private val stateLiveData: MutableLiveData<S> = MutableLiveData()
+
+    fun getState(): LiveData<S> = stateLiveData
+
+    fun dispatch(action: A, onUnexpectedError: () -> Unit?): Flow<S> =
+        actionExecutor(action)
+            .scan(currentStateOrDefault()) { acc, value -> reducer.reduce(acc, value) }
+            .distinctUntilChanged()
+            .catch { error ->
+                emit(reducer.reduceUnexpectedError(currentStateOrDefault(), error))
+                onUnexpectedError()
+            }
+            .flowOn(background)
+            .onEach { stateLiveData.value = it }
+
+    private fun currentStateOrDefault() = stateLiveData.value ?: initState
+}
--- app/src/main/java/com/github/glomadrian/architecture/ViewModel.kt
+++ app/src/main/java/com/github/glomadrian/architecture/ViewModel.kt
@@ -1,47 +1,30 @@
 package com.github.glomadrian.architecture
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.glomadrian.background
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapMerge
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.scan
 
-abstract class ViewModel<I : Intent, S : State, A : Action, R : Result> (
-    private val actionExecutor: ActionExecutor<A, R>,
-    private val reducer: Reducer<R, S>
-): ViewModel() {
+abstract class ViewModel<I : Intent, S : State, A : Action, R : Result>(
+    actionExecutor: ActionExecutor<A, R>,
+    reducer: Reducer<R, S>
+) : ViewModel() {
 
-    private val stateLiveData: MutableLiveData<S> = MutableLiveData()
+    private val store = Store(actionExecutor, reducer, initialState())
 
     fun processIntents(intents: Flow<I>) {
         intents
             .map { handleIntent(it) }
-            .flatMapMerge { actionExecutor(it) }
-            .scan(currentStateOrDefault()) { acc, value -> reducer.reduce(acc, value) }
-            .distinctUntilChanged()
-            .catch { error ->
-                emit(reducer.reduceUnexpectedError(currentStateOrDefault(), error))
-                processIntents(intents)
-            }
-            .flowOn(background)
-            .onEach { stateLiveData.value = it }
+            .flatMapMerge { store.dispatch(it, { processIntents(intents) }) }
             .launchIn(viewModelScope)
     }
 
-    private fun currentStateOrDefault() = stateLiveData.value ?: initialState()
-
     abstract suspend fun handleIntent(intent: I): A
 
-    fun state(): LiveData<S> = stateLiveData
+    fun state(): LiveData<S> = store.getState()
 
     abstract fun initialState(): S
 }
